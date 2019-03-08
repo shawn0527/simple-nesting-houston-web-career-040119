5dea66ec6979b526d6a18c65ab841eb08aaab937
Shawn Ren <doggy-ren@hotmail.com>
2019-03-08 22:52:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:alan_kay]
+		 return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def dennis_ritchies_language
